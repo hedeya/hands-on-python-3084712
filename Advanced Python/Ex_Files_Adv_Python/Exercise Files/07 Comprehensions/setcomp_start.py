4a34c197684547c58d89322826c65c09945d70d6
hedeya <haytham.salah@gmail.com>
2022-12-16 19:57:43 +0000

    Comprehensions code

--- Advanced Python/Ex_Files_Adv_Python/Exercise Files/07 Comprehensions/setcomp_start.py
+++ Advanced Python/Ex_Files_Adv_Python/Exercise Files/07 Comprehensions/setcomp_start.py
@@ -4,10 +4,17 @@
 def main():
     # define a list of temperature data points
     ctemps = [5, 10, 12, 14, 10, 23, 41, 30, 12, 24, 12, 18, 29]
+    ftemps1 = [(t*9/5)+32 for t in ctemps]
+    ftemps2 = {(t*9/5)+32 for t in ctemps}
 
+    print(ftemps1)
+    print(ftemps2)
     # TODO: build a set of unique Fahrenheit temperatures
 
     # TODO: build a set from an input source
+    cTemp = "The quick brown fox jumped over the lazy dog"
+    cUp = {c.upper() for c in cTemp if not c.isspace()}
+    print(cUp)
 
 if __name__ == "__main__":
     main()
